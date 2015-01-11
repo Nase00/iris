ddb9c74c38aef4dffb83f5c58504de268fb49391
nase00 <nase00@gmail.com>
2015-01-11 02:46:07 -0600

    Fixed DB name

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+module Seed
+  def self.run
+    User.create!(name: "Sean", email: "nase00@gmail.com", twitter: "@NaseZero", password: "test", is_admin: true)
+    User.create!(name: "testUser1", email: "testUser1@whatever.com", twitter: "@testUser1", password: "test1", is_admin: false)
+    User.create!(name: "testUser2", email: "testUser2@whatever.com", twitter: "@testUser2", password: "test2", is_admin: false)
+
+    25.times do # Seed blogs
+      title = Faker::Lorem.word
+      description = Faker::Lorem.sentence(5)
+      content = Faker::Lorem.sentence(200)
+      author_id = rand(1..3)
+      Blog.create(title: title, description: description, content: content, author_id: author_id)
+    end
+
+    100.times do # Seed comments
+      title = Faker::Lorem.word
+      content = Faker::Lorem.sentence(50)
+      blog_id = rand(1..20)
+      commenter_id = rand(1..3)
+      Comment.create(title: title, content: content, blog_id: blog_id, commenter_id: commenter_id)
+    end
+
+    25.times do # Seed galleries
+      title = Faker::Lorem.word
+      description = Faker::Lorem.sentence(5)
+      creator_id = rand(1..3)
+      Gallery.create(title: title, description: description, creator_id: creator_id)
+    end
+
+    20.times do # Seed projects
+      title = Faker::Lorem.word
+      content = Faker::Lorem.sentence(10)
+      gallery_id = rand(1..5)
+      Project.create(title: title, content: content, gallery_id: gallery_id)
+    end
+  end
+end
